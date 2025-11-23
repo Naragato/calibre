dedeb7524fdabb85b6b1bcde623201f949cc4861
Simon Balarabe <development@simonbalarabe.com>
2025-11-23 14:30:11 +0100

    feat: Allow '+' in names

--- src/calibre/__init__.py
+++ src/calibre/__init__.py
@@ -127,7 +127,7 @@ def confirm_config_name(name):
 
 
 _filename_sanitize_unicode = frozenset(('\\', '|', '?', '*', '<',        # no2to3
-    '"', ':', '>', '+', '/') + tuple(map(chr, range(32))))  # no2to3
+    '"', ':', '>', '/') + tuple(map(chr, range(32))))  # no2to3
 
 
 def sanitize_file_name(name, substitute='_'):
